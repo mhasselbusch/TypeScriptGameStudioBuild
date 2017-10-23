689fd67f7fb9883a6e05e15ee914de253739d947
Maximilian Hasselbusch <MaxHasselbusch@MaxBook-Pro.local>
2017-10-23 17:51:59 -0400

    test

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 var express = require('express');
+var fs = require('fs');
 const { spawn } = require('child_process');
 var app = express();
+
+var increment = 0;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send(increment)
+  increment++;
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
+});
+
+app.post("/compile", function(){
+
+
+
 });
 
 /*
