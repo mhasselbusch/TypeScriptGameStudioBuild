df32ac883d8d979f3f3ae7fc36cc6c07151017c9
Maximilian Hasselbusch <MaxHasselbusch@MaxBook-Pro.local>
2017-10-23 17:56:53 -0400

    test

--- server.js
+++ server.js
@@ -2,24 +2,23 @@ var express = require('express');
 var fs = require('fs');
 const { spawn } = require('child_process');
 var app = express();
-
-var increment = 0;
+var str = "";
+var compileQ = [];
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(increment)
-  increment++;
+  response.send('Hello World!' + str);
 });
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
 
-app.post("/compile", function(){
+app.post("/compile", function(req, res){
 
-
+	console.log(req.body);
 
 });
 
@@ -32,6 +31,7 @@ const ls = spawn('ls', ['-lh', './']);
 
 ls.stdout.on('data', (data) => {
   console.log(`stdout: ${data}`);
+  str = `stdout: ${data}`;
 });
 
 ls.stderr.on('data', (data) => {
