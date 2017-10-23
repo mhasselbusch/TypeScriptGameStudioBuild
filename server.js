0f8fd1688707a1b03263198cf658f3b4fd220db5
Maximilian Hasselbusch <MaxHasselbusch@MaxBook-Pro.local>
2017-10-23 19:20:35 -0400

    further improvements

--- server.js
+++ server.js
@@ -1,38 +1,53 @@
+/*============
+
+  Module definitions
+
+==============*/
+
+//App modules
 var express = require('express');
 var fs = require('fs');
-const { spawn } = require('child_process');
+var app = module.exports = express();
 var bodyParser = require('body-parser');
 
-var app = express();
+const { spawn } = require('child_process');
+
+/*============
+
+  Server Setup
+
+==============*/
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-var str = "";
-var compileQ = [];
-
-
-app.listen(process.env.PORT || 5000, function () {
-	console.log("App now running on port", process.env.PORT || 5000);
-});
-
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!' + str);
+var compileQ = [];
+var verifyURL = process.env.VERIFY_URL;
+app.listen(process.env.PORT || 5001, function () {
+	console.log("App now running on port", process.env.PORT || 5001);
 });
+
+/*============
+  
+  Server Endpoints
+
+==============*/
 
 app.post("/compile", function(req, res){
 
-	console.log(req.body.content);
-
+	console.log(req.body.email);
+	res.write(200);
+	res.end();
 });
 
-/*
+/*============
+	
 	Quick sample spawning of child process to run the ls command.
 
 	In the future, we will spawn child processes to run tsc on typescript files
-*/
+
+============*/
 const ls = spawn('ls', ['-lh', './']);
 
 ls.stdout.on('data', (data) => {
