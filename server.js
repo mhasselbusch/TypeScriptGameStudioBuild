4a2c539640ec7db1d05257bdad38e3dbd98eaae9
Maximilian Hasselbusch <MaxHasselbusch@dyn133240.cc.lehigh.edu>
2017-10-26 18:19:30 -0400

    prep

--- server.js
+++ server.js
@@ -6,10 +6,11 @@
 
 //App modules
 var express = require('express');
+var https = require("https");
 var fs = require('fs');
 var app = module.exports = express();
+var mongodb = require('mongodb');
 var bodyParser = require('body-parser');
-
 const { spawn } = require('child_process');
 
 /*============
@@ -18,15 +19,34 @@ const { spawn } = require('child_process');
 
 ==============*/
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 var compileQ = [];
 var verifyURL = process.env.VERIFY_URL;
-app.listen(process.env.PORT || 5001, function () {
-	console.log("App now running on port", process.env.PORT || 5001);
+var verifyLocalURL = process.env.VERIFY_LOCALURL;
+var database_url = process.env.MONGODB_URI;
+
+mongodb.MongoClient.connect(database_url, function(err, db) {
+
+	if(err){
+		console.log(err)
+		process.exit(1);	
+	}
+
+	connection = db;
+
+	//Export the database connection and the database module for use throughout the app
+	module.exports.connection = connection;
+
+	console.log("We are connected to the database");
+
+	app.listen(5001, function () {
+		console.log("App now running on port", 5001);
+	});
 });
+
 
 /*============
   
@@ -35,10 +55,22 @@ app.listen(process.env.PORT || 5001, function () {
 ==============*/
 
 app.post("/compile", function(req, res){
-
-	console.log(req.body.email);
-	res.write(200);
-	res.end();
+	
+  if(req.user){
+	  connection.collection('accounts').findOne({
+	    user_email : req.body.email
+	  }, function(err, object){
+	    if(!err){ 
+	      res.write(JSON.stringify(object));
+	      res.status(200);
+	      res.end();
+	    }
+	    else{
+	      res.status(500);
+	      res.end();
+	    }
+	  });
+  }
 });
 
 /*============
@@ -48,6 +80,7 @@ app.post("/compile", function(req, res){
 	In the future, we will spawn child processes to run tsc on typescript files
 
 ============*/
+
 const ls = spawn('ls', ['-lh', './']);
 
 ls.stdout.on('data', (data) => {
