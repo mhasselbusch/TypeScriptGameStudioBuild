ad995290fa257124131714b017f0bbb47834c46f
Maximilian Hasselbusch <MaxHasselbusch@MaxBook-Pro.local>
2017-11-17 00:42:29 -0500

    last few adjustments

--- compilations/library/CollisionCallback.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-//// <reference path="./WorldActor.ts" />
-//// <reference path="./typedefinitions/physicstype2d/PhysicsType2d.v0_9.d.ts"/>
-//// <reference path="./typedefinitions/pixi.js/index.d.ts"/>
-// <reference types="pixi.js"/>
